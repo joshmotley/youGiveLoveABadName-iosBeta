1fd7e180d8d3a07fd636c2466faede6f0020267a
joshmotley <joshkmotley@gmail.com>
2016-01-26 21:22:08 -0500

    complete bon jovi animations using UIKeyFrameAnimations

--- youGiveLoveABadName/FISViewController.m
+++ youGiveLoveABadName/FISViewController.m
@@ -11,6 +11,7 @@
 @interface FISViewController ()
 @property (strong, nonatomic) IBOutlet UIImageView *bonJovi;
 - (IBAction)expand:(id)sender;
+@property BOOL isExpanded;
 
 @end
 
@@ -19,6 +20,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.isExpanded = NO;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -29,5 +32,72 @@
 }
 
 - (IBAction)expand:(id)sender {
+    
+    if (self.isExpanded == NO) {
+
+
+    
+    
+    [UIView animateKeyframesWithDuration:1.0
+                                   delay:0.0
+                                 options:UIViewKeyframeAnimationOptionCalculationModeLinear
+                              animations:^{
+                                  [UIView addKeyframeWithRelativeStartTime:0.0
+                                                          relativeDuration:1/3.0
+                                                                animations:^{
+                                                                    self.bonJovi.frame = CGRectMake(0, self.bonJovi.frame.origin.y+20, self.bonJovi.frame.size.width, self.bonJovi.frame.size.height-40);
+                                                                }];
+                                  
+                                  [UIView addKeyframeWithRelativeStartTime:0.25
+                                                          relativeDuration:1/3.0
+                                                                animations:^{
+                                                                    self.bonJovi.frame = CGRectMake(0, -20, self.bonJovi.frame.size.width, self.view.frame.size.height+40);
+                                                                }];
+                                  [UIView addKeyframeWithRelativeStartTime:0.66
+                                                          relativeDuration:1/3.0
+                                                                animations:^{
+                                                                    self.bonJovi.frame = CGRectMake(0, 0, self.bonJovi.frame.size.width, self.view.frame.size.height);
+                                                                }];
+                                  
+                              }
+                              completion:^(BOOL finished) {
+                                  
+                                  self.isExpanded = YES;
+                                  
+                                 
+                              }];
+        
+    }else{
+        
+        [UIView animateKeyframesWithDuration:1.0
+                                       delay:0.0
+                                     options:UIViewKeyframeAnimationOptionCalculationModeLinear
+                                  animations:^{
+
+        [UIView addKeyframeWithRelativeStartTime:0.0
+                                                              relativeDuration:1/3.0
+                                                                    animations:^{
+                                                                        self.bonJovi.frame = CGRectMake(0, -20, self.bonJovi.frame.size.width, self.view.frame.size.height+40);
+                                                                    }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.25
+                                relativeDuration:1/3.0
+                                      animations:^{
+                                          self.bonJovi.frame = CGRectMake(0, 217, self.bonJovi.frame.size.width, 134);
+                                      }];
+        
+
+        [UIView addKeyframeWithRelativeStartTime:0.66
+                                relativeDuration:1/3.0
+                                      animations:^{
+                                          self.bonJovi.frame = CGRectMake(0, 197, 320, 174);
+                                      }];
+                                  }
+                                  completion:^(BOOL finished) {
+                                      self.isExpanded = NO;
+                                      
+                                      
+                                  }];
+    }
 }
 @end
